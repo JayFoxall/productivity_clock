d0456735d33c312f86f0cbb464a98850e8e73c77
jay <jayfoxall@hotmail.co.uk>
2023-04-03 21:40:39 +0100

    first commit

---
+++ src/components.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export function ProductivityClock(){
+    return (
+        <>
+            <Timer/>
+            <Loops/>
+        </>
+    )
+}
+
+export function Timer(){
+    return (
+        <>
+            12345
+        </>
+    )
+}
+
+export function Loops(){
+    return(
+        <>
+        "Loopcount"
+        </>
+    )
+}
