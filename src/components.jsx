7d9be15c68274f7695758fff742c7bcf1576e5fd
jay <jayfoxall@hotmail.co.uk>
2023-04-19 01:40:11 +0100

    Complete, Tests Failing

--- src/components.jsx
+++ src/components.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export function ProductivityClock() {
   const INCREMENT = "Increment";
@@ -6,80 +6,93 @@ export function ProductivityClock() {
   const SESSION = "Session";
   const BREAK = "Break";
 
-  const defaultTimerState = { sessionLength: 25, breakLength: 5 };
+  const defaultTimerState = { sessionLength: 1, breakLength: 5 };
+  let [timer, setTimer] = useState(defaultTimerState);
+
+  const defaultDisplayState = {
+    minutesRemaining: timer.sessionLength,
+    secondsRemaining: "00",
+  };
+  let [displayState, setDisplayState] = useState(defaultDisplayState);
+
   const defaultPlayState = {
     countdown: false,
     activeTimer: SESSION,
     timerID: 0,
   };
-
-  let [display, setDisplay] = useState(defaultTimerState);
   let [playState, setPlayState] = useState(defaultPlayState);
 
   function adjustTimeClickHandler(component, element, state) {
     if (element.innerHTML === INCREMENT) {
-      if (component === "SessionTimer") {
-        setDisplay(Object.assign({}, display, { sessionLength: (state += 1) }));
-      } else {
-        setDisplay(Object.assign({}, display, { breakLength: (state += 1) }));
+      if (component === "SessionTimer" && timer.sessionLength < 60) {
+        setTimer(Object.assign({}, timer, { sessionLength: (state += 1) }));
+      } else if (component === "BreakTimer" && timer.breakLength < 60) {
+        setTimer(Object.assign({}, timer, { breakLength: (state += 1) }));
       }
     } else if (element.innerHTML === DECREMENT)
-      if (component === "SessionTimer") {
-        setDisplay(Object.assign({}, display, { sessionLength: (state -= 1) }));
-      } else {
-        setDisplay(Object.assign({}, display, { breakLength: (state -= 1) }));
+      if (component === "SessionTimer" && timer.sessionLength > 1) {
+        setTimer(Object.assign({}, timer, { sessionLength: (state -= 1) }));
+      } else if (component === "BreakTimer" && timer.breakLength > 1) {
+        setTimer(Object.assign({}, timer, { breakLength: (state -= 1) }))
+        setPlayState(Object.assign({}, playState, {activeTimer: BREAK }));
+        console.log(playState)
       }
   }
 
   //TODO: Refactor clockCountdown to return minutesRemaining and secondsRemaining, and pass in break/sessionTimeInSeconds as arguments
-  let sessionTimeInSeconds = display.sessionLength * 60;
-  let breakTimeInSeconds = display.breakLength * 60;
-  let minutesRemaining = 0;
-  let secondsRemaining = 0;
+  let sessionTimeInSeconds = timer.sessionLength * 60;
+  let breakTimeInSeconds = timer.breakLength * 60;
+  let minutes_Remaining = 0;
+  let seconds_Remaining = 0;
 
   function clockCountdown() {
     if (playState.activeTimer === SESSION) {
       if (sessionTimeInSeconds > 0) {
         sessionTimeInSeconds--;
-        minutesRemaining = Math.floor(sessionTimeInSeconds / 60);
-        secondsRemaining = sessionTimeInSeconds % 60;
-        console.log(minutesRemaining + ":" + secondsRemaining);
-        this.forceUpdate();
+        minutes_Remaining = Math.floor(sessionTimeInSeconds / 60);
+        seconds_Remaining = sessionTimeInSeconds % 60;
+        console.log(minutes_Remaining + ":" + seconds_Remaining, playState);
+        setDisplayState({
+          minutesRemaining: minutes_Remaining,
+          secondsRemaining: seconds_Remaining,
+        });
       }
-      if (sessionTimeInSeconds === 0) {
-        setPlayState(Object.assign({}, playState, { activeTimer: BREAK }));
-        sessionTimeInSeconds = display.sessionLength * 60;
-      }
+    if (sessionTimeInSeconds === 0) {
+      setPlayState(Object.assign({}, playState, {activeTimer: BREAK }));
+      return
     }
+  }
+
     if (playState.activeTimer === BREAK) {
       if (breakTimeInSeconds > 0) {
         breakTimeInSeconds--;
-        minutesRemaining = Math.floor(breakTimeInSeconds / 60);
-        secondsRemaining = breakTimeInSeconds % 60;
-        console.log(minutesRemaining + ":" + secondsRemaining);
-        this.forceUpdate();
+        minutes_Remaining = Math.floor(breakTimeInSeconds / 60);
+        seconds_Remaining = breakTimeInSeconds % 60;
+        console.log(minutes_Remaining + ":" + seconds_Remaining);
+        setDisplayState({
+          minutesRemaining: minutes_Remaining,
+          secondsRemaining: seconds_Remaining
+        });
       }
       if (breakTimeInSeconds === 0) {
-        setPlayState(Object.assign({}, playState, { activeTimer: SESSION }));
-        breakTimeInSeconds = display.sessionLength * 60;
+        setPlayState(Object.assign({}, playState, {activeTimer: SESSION }));
+        breakTimeInSeconds = timer.sessionLength * 60;
+        
       }
     }
   }
 
-  // function activateTimer(){
-  //    return setInterval(() => clockCountdown(), 1000);
-  // }
-  // function stopTimer() {
-  //   clearInterval();
-  // }
-
   let id = [];
   function startStopClickHandler() {
     if (playState.countdown === false) {
-      id = setInterval(() => clockCountdown(), 1000);
+      
+      id = setInterval(() => {clockCountdown()}, 100);
       setPlayState(
         Object.assign({}, playState, { countdown: true, timerID: id })
       );
+      console.log(playState)
+      id = setInterval(() => clockCountdown(), 100)
+      
     } else {
       setPlayState(Object.assign({}, playState, { countdown: false }));
       clearInterval(playState.timerID);
@@ -89,23 +102,38 @@ export function ProductivityClock() {
   return (
     <>
       <h1>Productivity Clock</h1>
-      <SessionTimer display={display} TimeClick={adjustTimeClickHandler} />
-      <BreakLength display={display} TimeClick={adjustTimeClickHandler} />
-      <section>
-        <div id="timer-label">session:</div>
-        <div id="time=left">
-          {playState.countdown === false
-            ? `${display.sessionLength}:00`
-            : `${minutesRemaining} : ${secondsRemaining}`}
-        </div>
-        <button id="start-stop" onClick={startStopClickHandler}>
-          Start/Stop
-        </button>
-        <button id="reset" onClick={() => setDisplay(defaultTimerState)}>
-          reset
-        </button>
-      </section>
+      <SessionTimer timer={timer} adjustTime={adjustTimeClickHandler} />
+      <BreakLength timer={timer} adjustTime={adjustTimeClickHandler} />
+      <Display
+        playState={playState}
+        timer={timer}
+        minutesRemaining={displayState.minutesRemaining}
+        secondsRemaining={displayState.secondsRemaining}
+        startStopClickHandler={startStopClickHandler}
+      />
     </>
+  );
+}
+
+export function Display(props) {
+  function resetClickHandler() {
+    clearInterval(props.playState.timerID);
+    props.timer.setTimer(props.defaultTimerState);
+  }
+
+  return (
+    <section>
+      <div id="timer-label">session:</div>
+      <div id="time=left">
+        {`${props.minutesRemaining} : ${props.secondsRemaining}`}
+      </div>
+      <button id="start-stop" onClick={props.startStopClickHandler}>
+        Start/Stop
+      </button>
+      <button id="reset" onClick={() => resetClickHandler}>
+        reset
+      </button>
+    </section>
   );
 }
 
@@ -115,14 +143,14 @@ export function SessionTimer(props) {
   return (
     <div id="session-label">
       <p>Session Length</p>
-      <section id="session-length"> {props.display.sessionLength}:00 </section>
+      <section id="session-length"> {props.timer.sessionLength}:00 </section>
       <button
         id="session-increment"
         onClick={() =>
-          props.TimeClick(
+          props.adjustTime(
             SESSION_COMPONENT,
             document.getElementById("session-increment"),
-            props.display.sessionLength
+            props.timer.sessionLength
           )
         }
       >
@@ -131,10 +159,10 @@ export function SessionTimer(props) {
       <button
         id="session-decrement"
         onClick={() =>
-          props.TimeClick(
+          props.adjustTime(
             SESSION_COMPONENT,
             document.getElementById("session-decrement"),
-            props.display.sessionLength
+            props.timer.sessionLength
           )
         }
       >
@@ -149,14 +177,14 @@ export function BreakLength(props) {
   return (
     <div id="break-label">
       <p>Break Length</p>
-      <section id="break-length"> {props.display.breakLength}:00 </section>
+      <section id="break-length"> {props.timer.breakLength}:00 </section>
       <button
         id="break-increment"
         onClick={() =>
-          props.TimeClick(
+          props.adjustTime(
             BREAK_COMPONENT,
             document.getElementById("break-increment"),
-            props.display.breakLength
+            props.timer.breakLength
           )
         }
       >
@@ -165,10 +193,10 @@ export function BreakLength(props) {
       <button
         id="break-decrement"
         onClick={() =>
-          props.TimeClick(
+          props.adjustTime(
             BREAK_COMPONENT,
             document.getElementById("break-decrement"),
-            props.display.breakLength
+            props.timer.breakLength
           )
         }
       >
